600deb9e84c5a6e82b8f48b938859c1449db18ae
philip <philip.k@cottagelabs.com>
2024-04-01 11:29:37 +0100

    extract updateFormValue

--- site_front_2/flaskapp/static/js/emfr/emfrUtils.js
+++ site_front_2/flaskapp/static/js/emfr/emfrUtils.js
@@ -2,3 +2,7 @@ export function metaContent(name) {
     const meta = document.querySelector(`meta[name="${name}"]`);
     return meta ? meta.content : null;
 }
+
+export function updateFormValue(formSelector, field, vlaue) {
+    document.querySelector(`${formSelector} input[name="${field}"]`).value = vlaue;
+}

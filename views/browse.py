2317ddc9381afdb47f4d4d97decbd6805861b653
kush.varade.19 <kush.varade.19@gmail.com>
2024-07-24 11:06:07 +0530

    Browse nesting options added

--- views/browse.py
+++ views/browse.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
-browse_bp = Blueprint('browse', __name__)
+browse_bp = Blueprint('browse', __name__ , url_prefix='/browse')
 
-@browse_bp.route('/browse')
+@browse_bp.route('/')
 def browse():
-    return render_template('browse.jinja2', title="Browse")
+    return redirect(url_for('browse.people'))
+
+@browse_bp.route('/people')
+def people():
+    return render_template('/pages/browse/people.jinja2', title="Browse:People")
+
+@browse_bp.route('/location')
+def location():
+    return render_template('/pages/browse/location.jinja2', title="Browse:Location")

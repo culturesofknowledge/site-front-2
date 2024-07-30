7fc7afe001af3b17bdb14b62d50e6ecd59e107bc
kush.varade.19 <kush.varade.19@gmail.com>
2024-07-30 13:32:07 +0530

    Search functionality is been added

---
+++ views/forms.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, render_template, redirect, url_for, request
+
+forms_bp = Blueprint('forms', __name__ , url_prefix='/forms')
+
+@forms_bp.route('/')
+def forms():
+
+    print("I am here from forms")
+    people = request.form.get('people')
+    dat_from_year = request.form.get('dat_from_year')
+    dat_to_year = request.form.get('dat_to_year')
+    locations = request.form.get('locations')
+    let_con = request.form.get('let_con')
+
+    
+    # Construct the query parameters
+    query_params = {}
+    if people:
+        query_params['people'] = people
+    if dat_from_year:
+        query_params['dat_from_year'] = dat_from_year
+    if dat_to_year:
+        query_params['dat_to_year'] = dat_to_year
+    if locations:
+        query_params['locations'] = locations
+    if let_con:
+        query_params['let_con'] = dat_from_year
+    
+    # Redirect to the new URL with query parameters
+    return redirect(url_for('forms.results', **query_params))
+
+@forms_bp.route('/results')
+def results():
+    people = request.args.get('people')
+    locations = request.args.get('locations')
+
+    print("Received query parameters: field1=" + people)
+
+    return render_template('/results.jinja2', title="Results" , people=people, locations=locations)

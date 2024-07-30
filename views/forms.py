af9aa79de2c5b003cfb71b08133c3226aee48a73
kush.varade.19 <kush.varade.19@gmail.com>
2024-07-30 16:32:06 +0530

    Query params is been added

--- views/forms.py
+++ views/forms.py
@@ -2,17 +2,14 @@ from flask import Blueprint, render_template, redirect, url_for, request
 
 forms_bp = Blueprint('forms', __name__ , url_prefix='/forms')
 
-@forms_bp.route('/')
+@forms_bp.route('/', methods=['POST'])
 def forms():
-
-    print("I am here from forms")
     people = request.form.get('people')
     dat_from_year = request.form.get('dat_from_year')
     dat_to_year = request.form.get('dat_to_year')
     locations = request.form.get('locations')
     let_con = request.form.get('let_con')
 
-    
     # Construct the query parameters
     query_params = {}
     if people:
@@ -25,7 +22,7 @@ def forms():
         query_params['locations'] = locations
     if let_con:
         query_params['let_con'] = dat_from_year
-    
+
     # Redirect to the new URL with query parameters
     return redirect(url_for('forms.results', **query_params))
 
@@ -34,6 +31,6 @@ def results():
     people = request.args.get('people')
     locations = request.args.get('locations')
 
-    print("Received query parameters: field1=" + people)
+    print("Received query parameters: field1=")
 
     return render_template('/results.jinja2', title="Results" , people=people, locations=locations)

f05676acba22e54ee90ce68ba0723df86a4d17f4
kush.varade.19 <kush.varade.19@gmail.com>
2024-09-18 12:15:49 +0530

    An inital layout for result page is been created.

--- static/js/result.edges.js
+++ static/js/result.edges.js
@@ -1,28 +1,90 @@
 import emlo from "./edges.js";
 
 try {
+
+  // Fetching URL params
+  const params = window.location.params
+
+  console.log("params" , params)
+
   emlo.selector = "emlo-results";
   emlo.collection = "/solr/works/select";
+
+  // emlo.openingQuery = {
+  //   must: [
+  //     { term: { author_sort: "Newton" } }, 
+  //   ],
+  // }
   emlo.components = [
+    new edges.components.Pager({
+      id: "top-pager",
+      category: "top-pager",
+      renderer: new edges.renderers.bs3.Pager({
+        scrollSelector: "#results",
+        showSizeSelector: false,
+        showRecordCount: false,
+        showChevrons: true,
+      })
+    }),
+
+    new edges.components.Pager({
+      id: "bottom-pager",
+      category: "bottom-pager",
+      renderer: new edges.renderers.bs3.Pager({
+        scrollSelector: "#results",
+        showSizeSelector: false,
+        showRecordCount: false,
+        showChevrons: true,
+      })
+    }),
+
     new emlo.ResultTable({
       id: "results",
       category: "results",
       secondaryResults: false,
       infiniteScroll: true,
-      infiniteScrollPageSize: 10,
+      infiniteScrollPageSize: 50,
       renderer: new emlo.ResultTableRenderer({
         noResultsText: "No results to display",
         serialHeader: "",
         tableDisplay: [
           {
-            header: "Cito catalog",
+            header: "Date",
             field: "cito_Catalog",
             pre: '<a href="mailto:',
             post: '">',
             valueFunction: null,
           },
           {
-            header: "Name",
+            header: "Author",
+            field: "author_sort",
+            pre: "",
+            post: "",
+            valueFunction: null,
+          },
+          {
+            header: "Origin",
+            field: "author_sort",
+            pre: "",
+            post: "",
+            valueFunction: null,
+          },
+          {
+            header: "Addressee",
+            field: "author_sort",
+            pre: "",
+            post: "",
+            valueFunction: null,
+          },
+          {
+            header: "Destination",
+            field: "author_sort",
+            pre: "",
+            post: "",
+            valueFunction: null,
+          },
+          {
+            header: "Repositories & Versions",
             field: "author_sort",
             pre: "",
             post: "",

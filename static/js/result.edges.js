0da867fb083ec88779e746bca56a85a51ccfaed3
kush.varade.19 <kush.varade.19@gmail.com>
2024-09-19 15:34:54 +0530

    Interlinking of search page is added

--- static/js/result.edges.js
+++ static/js/result.edges.js
@@ -7,13 +7,36 @@ try {
 
   if(queryString) {
     const params = new URLSearchParams(queryString)
-    console.log("params" , typeof params)
+   
     emlo.openingQuery = {
       must: [],
     }
+
     const people = params.get("people")
-    
+    const fromDate = params.get("dat_from_year")
+    const toDate = params.get("dat_to_year")
+    const locations = params.get("locations")
+    const content = params.get("let_con")
+
     if(people) {
+      emlo.openingQuery.must.push({
+        term : { "author_sort" : people }
+      })
+    }
+
+    if(fromDate && toDate) {
+      emlo.openingQuery.must.push({
+        term : { "author_sort" : people }
+      })
+    }
+
+    if(locations) {
+      emlo.openingQuery.must.push({
+        term : { "author_sort" : people }
+      })
+    }
+
+    if(content) {
       emlo.openingQuery.must.push({
         term : { "author_sort" : people }
       })
@@ -24,11 +47,6 @@ try {
   emlo.selector = "emlo-results";
   emlo.collection = "/solr/works/select";
 
-  // emlo.openingQuery = {
-  //   must: [
-  //     { term: { author_sort: "Newton" } }, 
-  //   ],
-  // }
   emlo.components = [
     new edges.components.Pager({
       id: "top-pager",

a2f3cad728135163f14b61a783ec42eb6b5b93d6
kush.varade.19 <kush.varade.19@gmail.com>
2024-09-19 12:43:44 +0530

    Modify search for dates is enabled

--- views/advance.py
+++ views/advance.py
@@ -6,5 +6,11 @@ advance_bp = Blueprint('advance', __name__)
 def advance():
     people = request.args.get('people')
     locations = request.args.get('locations')
+    dat_from_year = request.args.get('dat_from_year' , 'all years')
+    dat_to_year = request.args.get('dat_to_year' , 'all years')
 
-    return render_template('advance.jinja2', title="Search +", people=people, locations=locations)
+    return render_template('advance.jinja2', title="Search +", 
+                           people=people, 
+                           locations=locations, 
+                           dat_from_year=dat_from_year, 
+                           dat_to_year=dat_to_year)

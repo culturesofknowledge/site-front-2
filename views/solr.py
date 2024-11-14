e6a665788d5c0a91a20004c756ac06a4beed15e2
kush.varade.19 <kush.varade.19@gmail.com>
2024-11-14 17:29:11 +0530

    Running of UI deattaching with docker

--- views/solr.py
+++ views/solr.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 import requests
 import os
 from dotenv import load_dotenv
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlencode
 
 load_dotenv()
 
@@ -17,13 +17,22 @@ if not SOLR_URL:
 @solr_bp.route('/solr/<path:subpath>', methods=['GET'])  # Include methods you need
 def solr_proxy(subpath):
     try:
+        print(f"Got subpath as: {subpath}")
         # Construct the full URL for the external API request
-        full_url =  urljoin(SOLR_URL, f"solr/{subpath}")  # Append the captured subpath
+        full_url =  urljoin(SOLR_URL, f"solr/{subpath}")    # Append the captured subpath
 
-        # Forward the request to the external API
+        # Convert query parameters to a query string for logging
+        query_params = request.args.to_dict()
+        query_string = urlencode(query_params)
+        full_url_with_params = f"{full_url}?{query_string}" if query_string else full_url
+        
+        # Log the full URL with query parameters
+        print(f"Requesting URL: {full_url_with_params}")
+
+        # Make the request to the external Solr API, including query parameters
         response = requests.request(
-            method=request.method,  # Forward the original request method
-            url=full_url
+            method=request.method,
+            url=full_url_with_params    
         )
 
         # Raise an error if the external request failed

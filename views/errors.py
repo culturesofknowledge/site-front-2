655d9351bc04ce7006e219c8c0e730c6891d56f7
Anusha Ranganathan <anusha@cottagelabs.com>
2024-11-15 11:39:09 +0530

    Modified readme and fixed pep8 warnings

--- views/errors.py
+++ views/errors.py
@@ -2,9 +2,11 @@ from flask import Blueprint, render_template
 
 errors_bp = Blueprint('errors', __name__)
 
+
 @errors_bp.app_errorhandler(404)
 def route_not_found(err):
     return render_template('page_not_found.jinja2', title="Page not found"), 404
+
 
 @errors_bp.app_errorhandler(500)
 def internal_server_error(err):

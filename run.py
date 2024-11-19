5500b17ab8024bebb92a5b5a1f2e6b09722a0242
kush.varade.19 <kush.varade.19@gmail.com>
2024-11-19 15:12:42 +0530

    Base for profile page is created

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from views.collections import collections_bp
 from views.contribute import contribute_bp
 from views.about import about_bp
 from views.forms import forms_bp
+from views.profile import profile_bp
 from views.errors import errors_bp
 from views.solr import solr_bp
 from config import Config
@@ -22,6 +23,7 @@ def create_app():
     app.register_blueprint(contribute_bp)
     app.register_blueprint(about_bp)
     app.register_blueprint(forms_bp)
+    app.register_blueprint(profile_bp)
     app.register_blueprint(errors_bp)
     app.register_blueprint(solr_bp)
 
---
+++ views/profile.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+profile_bp = Blueprint('profile', __name__, url_prefix='/profile')
+
+@profile_bp.route('/<collection>/<id>')
+def profile(collection, id):
+    return render_template('pages/profile.jinja2', title="Profile", collection=collection, id=id)

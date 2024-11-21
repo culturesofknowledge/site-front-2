70544fa81b9ec77bddadf42faf89552f8f6d8a44
kush.varade.19 <kush.varade.19@gmail.com>
2024-11-21 12:59:45 +0530

    Profile page base creation

--- views/profile.py
+++ views/profile.py
@@ -4,4 +4,4 @@ profile_bp = Blueprint('profile', __name__, url_prefix='/profile')
 
 @profile_bp.route('/<collection>/<id>')
 def profile(collection, id):
-    return render_template('pages/profile.jinja2', title="Profile", collection=collection, id=id)
+    return render_template('profile.jinja2', title="Profile", collection=collection, id=id)

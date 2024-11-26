e47d87ef00a6809d1dae6a72bdbe446951a264df
kush.varade.19 <kush.varade.19@gmail.com>
2024-11-26 08:05:32 +0530

    Stats is been added to the home page

--- static/js/home.edges.js
+++ static/js/home.edges.js
@@ -1,38 +1,118 @@
-import emloEdges from "./edges.js";
+import emlo from "./edges.js";
 
 try {
-  emloEdges.selector = "home-search";
-  emloEdges.collection = "/solr/works/select";
-  emloEdges.components = [
-    new edges.components.FullSearchController({
-      id: "search-controller",
-      category: "controller",
-      defaultOperator: "AND",
-      defaultField: "author_sort",
-      renderer: new edges.renderers.bs5.SearchBox({
-        freetextSubmitDelay: -1,
-        clearButton: false,
-        searchPlaceholder: "all people",
+  emlo.selector = "home-stats";
+  emlo.collection = "/solr/all/select";
+  emlo.template = new emlo.HomeStatsTemplate();
+  emlo.components = [
+    new emlo.Stats({
+      id: "people",
+      category: "stats",
+      solrCore: "people",
+      renderer: new emlo.StatsRenderer({
+        title: "people",
+        titleImage: "/static/img/icon-stats-people.png",
+        redirectURL: "/browse/people",
       }),
     }),
-    new edges.components.ResultsDisplay({
-      id: "results",
-      category: "results",
-      renderer: new edges.renderers.bs3.ResultsFieldsByRow({
-        rowDisplay: [
-          [{ field: "cito_Catalog" }],
-          [
-            {
-              pre: "<strong>Editor Group</strong>: ",
-              field: "author_sort",
-            },
-          ],
-        ],
+
+    new emlo.Stats({
+      id: "locations",
+      category: "stats",
+      solrCore: "locations",
+      renderer: new emlo.StatsRenderer({
+        title: "Location",
+        titleImage: "/static/img/icon-stats-locations.png",
+        redirectURL: "/browse/locations",
+      }),
+    }),
+
+    new emlo.Stats({
+      id: "organisations",
+      category: "stats",
+      solrCore: "institutions",
+      renderer: new emlo.StatsRenderer({
+        title: "Organisations",
+        titleImage: "/static/img/icon-stats-organisations.png",
+        redirectURL: "/browse/organisations",
+      }),
+    }),
+
+    new emlo.Stats({
+      id: "repositories",
+      category: "stats",
+      solrCore: "institutions",
+      renderer: new emlo.StatsRenderer({
+        title: "Repositories",
+        titleImage: "/static/img/icon-stats-repositories.png",
+        redirectURL: "/browse/institutions",
+      }),
+    }),
+
+    new emlo.Stats({
+      id: "catalogues",
+      category: "stats",
+      solrCore: "people",
+      renderer: new emlo.StatsRenderer({
+        title: "Catalogues",
+        titleImage: "/static/img/icon-stats-Catalogues.png",
+        redirectURL:
+          "http://emlo-portal.bodleian.ox.ac.uk/collections/?page_id=480",
+      }),
+    }),
+
+    new emlo.Stats({
+      id: "works",
+      category: "stats",
+      solrCore: "works",
+      renderer: new emlo.StatsRenderer({
+        title: "Letters",
+        titleImage: "/static/img/icon-stats-works.png",
+      }),
+    }),
+
+    new emlo.Stats({
+      id: "manifestations",
+      category: "stats",
+      solrCore: "manifestations",
+      renderer: new emlo.StatsRenderer({
+        title: "Manifestations",
+        titleImage: "/static/img/icon-stats-manifestations.png",
+      }),
+    }),
+
+    new emlo.Stats({
+      id: "images",
+      category: "stats",
+      solrCore: "images",
+      renderer: new emlo.StatsRenderer({
+        title: "Images",
+        titleImage: "/static/img/icon-stats-images.png",
+      }),
+    }),
+
+    new emlo.Stats({
+      id: "comments",
+      category: "stats",
+      solrCore: "comments",
+      renderer: new emlo.StatsRenderer({
+        title: "Comments",
+        titleImage: "/static/img/icon-stats-comments.png",
+      }),
+    }),
+
+    new emlo.Stats({
+      id: "resources",
+      category: "stats",
+      solrCore: "resources",
+      renderer: new emlo.StatsRenderer({
+        title: "Resources",
+        titleImage: "/static/img/icon-stats-related%20resources.png",
       }),
     }),
   ];
 
-  emloEdges.init();
+  emlo.init();
 } catch (error) {
   console.error(error.message);
 }

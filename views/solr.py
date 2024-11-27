0b7f641b936e4bc6c9c75adc09f5d8131006480e
Anusha Ranganathan <anusha@cottagelabs.com>
2024-11-27 10:47:30 +0530

    Debugging solr

--- views/solr.py
+++ views/solr.py
@@ -8,28 +8,31 @@ load_dotenv()
 
 solr_bp = Blueprint("solr", __name__)
 
-# External API endpoint you want to fetch data from
-SOLR_URL = os.getenv('SOLR_URL', '')
-
-if not SOLR_URL:
-    raise ValueError("SOLR_URL environment variable is not set. Please configure it before starting the app.")
-
-
 @solr_bp.route('/solr/<path:subpath>', methods=['GET'])  # Include methods you need
 def solr_proxy(subpath):
+    SOLR_URL = os.getenv('SOLR_URL', '')
+
+    if not SOLR_URL:
+        raise ValueError("SOLR_URL environment variable is not set. Please configure it before starting the app.")
+
+    # Construct the full URL for the external API request
+    # Append the captured subpath
+    print(f"Base URL: {SOLR_URL}")
+    print(f"Subpath: {subpath}")
+
+    if not SOLR_URL.endswith("/"):
+        SOLR_URL = SOLR_URL + "/"
+    subpath = subpath.lstrip('/')
+    full_url = SOLR_URL + subpath
+
+    print(f"Full URL: {full_url}")
+
+    query_string = request.query_string.decode("utf-8")
+    full_url_with_params = f"{full_url}?{query_string}"
+
+    print(f"Requesting URL: {full_url_with_params}")
+
     try:
-        # Construct the full URL for the external API request
-        # Append the captured subpath
-        if not SOLR_URL.endswith("/"):
-            SOLR_URL = SOLR_URL + "/"
-        subpath = subpath.lstrip('/')
-        full_url = SOLR_URL + subpath
-
-        query_string = request.query_string.decode("utf-8")
-        full_url_with_params = full_url
-
-        full_url_with_params = f"{full_url}?{query_string}"
-
         # Make the request to the external Solr API, including query parameters
         response = requests.request(
             method=request.method,
@@ -43,6 +46,7 @@ def solr_proxy(subpath):
         return jsonify(response.json()), response.status_code
 
     except requests.exceptions.HTTPError as http_err:
+        print(f"Got HTTP error: {http_err}")
         return jsonify({"error": str(http_err)}), http_err.response.status_code
     except Exception as err:
         print(f"Got error: {err}")

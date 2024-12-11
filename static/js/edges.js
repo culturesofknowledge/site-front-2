98f24c672a1dcf162c188f293fdf1d319ab14675
kush.varade.19 <kush.varade.19@gmail.com>
2024-12-11 09:02:19 +0530

    Pagination bug handled in case of start present in URL

--- static/js/edges.js
+++ static/js/edges.js
@@ -3373,10 +3373,10 @@ emlo.Pagination = class extends edges.Component {
       const start = parseInt(val);
       const from = parseInt(this.edge.currentQuery.getFrom());
 
-      if (start > from && start != 0) {
-        this.from = start + 1;
-      } else {
+      if (start != from) {
         this.from = parseInt(this.edge.currentQuery.getFrom()) + 1;
+      } else {
+        this.from = start + 1;
       }
 
       this.pageSize = parseInt(this.edge.currentQuery.getSize());

1df99d02517ae4ba082fe30615c21e6705fb937e
kush.varade.19 <kush.varade.19@gmail.com>
2024-12-26 13:24:36 +0530

    UI correction for selecting filters

--- views/browse.py
+++ views/browse.py
@@ -16,7 +16,7 @@ def people():
     # If filter is not present, add it with a default value (you can customize this value)
     if not filter_param:
         # Redirect with the filter query appended, this ensures the 'filter' param is always present
-        return redirect(url_for('browse.people', **{**request.args, 'filters': 'fe,ma,un'}))
+        return redirect(url_for('browse.people', **{**request.args, 'filters': 'fe,ma,un,re,wr,me'}))
     
     letter = request.args.get('letter', '').lower()
     return render_template('/pages/browse/people.jinja2', title="Browse:People" , letter=letter , filters=filter_param)

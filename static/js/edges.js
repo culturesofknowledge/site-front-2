6fdc29a93135ed459703b55b6dc321bbe76dc7d2
kush.varade.19 <kush.varade.19@gmail.com>
2024-12-27 07:20:09 +0530

    Browse missing images added

--- static/js/edges.js
+++ static/js/edges.js
@@ -1884,7 +1884,7 @@ emlo.MultiFieldsRenderer = class extends edges.Renderer {
   _renderSectionTitle() {
     if (this.component.results && this.component.results.length > 0) {
       const imageTag = this.sectionTitleImage
-        ? `<img src="${edges.util.escapeHtml(
+        ? `<img style="float:left;height:25px;width:25px;margin-right:15px;" src="${edges.util.escapeHtml(
             this.sectionTitleImage
           )}" alt="${edges.util.escapeHtml(
             this.sectionTitle
@@ -3906,7 +3906,7 @@ emlo.SortRenderer = class extends edges.Renderer {
 
     _addUrlParam("sort", selectedOption.value);
     // Trigger the sort logic (update the query or API call)
-    this.component.setSortBy(selectedOption.field);
+    this.component.synchronise(selectedOption.field);
   };
 };
 

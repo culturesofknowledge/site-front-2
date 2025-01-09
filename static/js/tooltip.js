b01f20b60b6310727b4b4a598402499823f982f1
kush.varade.19 <kush.varade.19@gmail.com>
2025-01-09 09:54:27 +0530

    Bug: Tooltip is displayed based on the space avaiable

--- static/js/tooltip.js
+++ static/js/tooltip.js
@@ -1,3 +1,55 @@
+// $(document).ready(function () {
+//   $("[data-tip]").each(function () {
+//     const $this = $(this);
+//     const delay = parseFloat($this.data("delay")) || 0; // Default delay to 0 if missing
+//     const tooltipContent = $this.data("tip"); // Get tooltip content
+
+//     // Create tooltip element
+//     const tooltip = $('<div class="tooltip"></div>').html(tooltipContent);
+//     $("body").append(tooltip); // Append to body for absolute positioning
+
+//     let timeout;
+
+//     // Function to show tooltip
+//     const showTooltip = (event, applyDelay = true) => {
+//       tooltip.css({
+//         top: event.pageY + 15 + "px", // Position tooltip 15px below pointer
+//         left: event.pageX + "px", // Position tooltip near pointer
+//       });
+//       if (applyDelay) {
+//         timeout = setTimeout(() => {
+//           tooltip.addClass("show");
+//         }, delay * 1000); // Apply delay
+//       } else {
+//         tooltip.addClass("show"); // No delay
+//       }
+//     };
+
+//     // Function to hide tooltip
+//     const hideTooltip = () => {
+//       clearTimeout(timeout);
+//       tooltip.removeClass("show");
+//     };
+
+//     // Bind events to both input and help icon
+//     const targets = $this.find("input, select, #help-icon");
+
+//     targets.on("mouseenter", function (event) {
+//       const isHelpIcon = $(this).is("#help-icon");
+//       showTooltip(event, !isHelpIcon); // No delay for #help-icon
+//     });
+
+//     targets.on("mousemove", (event) => {
+//       tooltip.css({
+//         top: event.pageY + 15 + "px",
+//         left: event.pageX + "px",
+//       });
+//     });
+
+//     targets.on("mouseleave", hideTooltip);
+//   });
+// });
+
 $(document).ready(function () {
   $("[data-tip]").each(function () {
     const $this = $(this);
@@ -12,10 +64,24 @@ $(document).ready(function () {
 
     // Function to show tooltip
     const showTooltip = (event, applyDelay = true) => {
+      let tooltipX = event.pageX;
+      let tooltipY = event.pageY + 15; // Position tooltip 15px below pointer
+
+      // Check if the tooltip overflows the right edge of the window
+      const windowWidth = $(window).width();
+      const tooltipWidth = tooltip.outerWidth();
+      const spaceLeft = windowWidth - tooltipX;
+
+      if (spaceLeft < tooltipWidth) {
+        // If there's not enough space on the right, position on the left
+        tooltipX = event.pageX - tooltipWidth - 15; // 15px from the pointer
+      }
+
       tooltip.css({
-        top: event.pageY + 15 + "px", // Position tooltip 15px below pointer
-        left: event.pageX + "px", // Position tooltip near pointer
+        top: tooltipY + "px", // Position tooltip
+        left: tooltipX + "px", // Position tooltip
       });
+
       if (applyDelay) {
         timeout = setTimeout(() => {
           tooltip.addClass("show");
@@ -40,10 +106,7 @@ $(document).ready(function () {
     });
 
     targets.on("mousemove", (event) => {
-      tooltip.css({
-        top: event.pageY + 15 + "px",
-        left: event.pageX + "px",
-      });
+      showTooltip(event, false); // Update position without delay
     });
 
     targets.on("mouseleave", hideTooltip);

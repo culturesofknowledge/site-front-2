d6eef5d0a80a2d05468dd964b6522db964f06957
kush.varade.19 <kush.varade.19@gmail.com>
2025-01-24 10:48:56 +0530

    Feedback-2 point 3.21 list for repo inside repo section is fixed and ordered

--- static/js/repository_dropdown.edges.js
+++ static/js/repository_dropdown.edges.js
@@ -7,6 +7,8 @@ emlo.components = [
   new emlo.DropDown({
     id: "repository_dropdown",
     category: "results",
+    size: 10000, // Fetching large number for drop down
+    sortOptions: [{ field: "browse", order: "asc" }],
     renderer: new emlo.DropDownRenderer({
       field: "geonames_officialName",
       defaultOptionText: "all repositories",

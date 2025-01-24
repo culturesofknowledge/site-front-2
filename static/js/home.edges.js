453aeefd8e4e9ae7b2c986ff9aec62d313e564b8
kush.varade.19 <kush.varade.19@gmail.com>
2025-01-24 13:28:16 +0530

    Feedback-1 count for home stats and it's aligment issue is been fixed

--- static/js/home.edges.js
+++ static/js/home.edges.js
@@ -6,110 +6,110 @@ try {
   emlo.template = new emlo.HomeStatsTemplate();
   emlo.components = [
     new emlo.Stats({
-      id: "people",
+      id: "stats",
       category: "stats",
-      solrCore: "people",
+      statsFields: [
+        "person",
+        "location",
+        "organizations",
+        "institution",
+        "cito_Catalog",
+        "work",
+        "manifestation",
+        "image",
+        "comment",
+        "resource",
+      ],
+      facetFields: [
+        { field: "ox_isOrganisation" },
+        { field: "object_type" },
+        { field: "cito_Catalog" },
+      ],
       renderer: new emlo.StatsRenderer({
-        title: "People",
-        titleImage: "/static/img/icon-stats-people.png",
-        redirectURL: "/browse/people",
-      }),
-    }),
+        statsEntries: [
+          {
+            title: "People",
+            titleImage: "/static/img/icon-stats-people.png",
+            redirectURL: "/browse/people",
+            statKey: "person",
+            tweakCount: 20097,
+            upperLimit: 21985,
+          },
+          {
+            title: "Location",
+            titleImage: "/static/img/icon-stats-locations.png",
+            redirectURL: "/browse/locations",
+            statKey: "location",
+            tweakCount: 5190,
+            upperLimit: 5931,
+          },
+          {
+            title: "Organizations",
+            titleImage: "/static/img/icon-stats-organisations.png",
+            redirectURL: "/browse/organisations",
+            statKey: "organizations",
+            tweakCount: 806,
+            upperLimit: 948,
+          },
+          {
+            title: "Repositories",
+            titleImage: "/static/img/icon-stats-repositories.png",
+            redirectURL: "/browse/institutions",
+            statKey: "institution",
+            tweakCount: 0,
+            upperLimit: 0,
+          },
 
-    new emlo.Stats({
-      id: "locations",
-      category: "stats",
-      solrCore: "locations",
-      renderer: new emlo.StatsRenderer({
-        title: "Location",
-        titleImage: "/static/img/icon-stats-locations.png",
-        redirectURL: "/browse/locations",
-      }),
-    }),
-
-    new emlo.Stats({
-      id: "organisations",
-      category: "stats",
-      solrCore: "all",
-      facetField: "ox_isOrganisation",
-      renderer: new emlo.StatsRenderer({
-        title: "‘Organizations",
-        titleImage: "/static/img/icon-stats-organisations.png",
-        redirectURL: "/browse/organisations",
-      }),
-    }),
-
-    new emlo.Stats({
-      id: "repositories",
-      category: "stats",
-      solrCore: "institutions",
-      renderer: new emlo.StatsRenderer({
-        title: "Repositories",
-        titleImage: "/static/img/icon-stats-repositories.png",
-        redirectURL: "/browse/institutions",
-      }),
-    }),
-
-    new emlo.Stats({
-      id: "catalogues",
-      category: "stats",
-      solrCore: "all",
-      facetField: "cito_Catalog",
-      renderer: new emlo.StatsRenderer({
-        title: "Catalogues",
-        titleImage: "/static/img/icon-stats-Catalogues.png",
-        redirectURL:
-          "http://emlo-portal.bodleian.ox.ac.uk/collections/?page_id=480",
-      }),
-    }),
-
-    new emlo.Stats({
-      id: "works",
-      category: "stats",
-      solrCore: "works",
-      renderer: new emlo.StatsRenderer({
-        title: "Letters",
-        titleImage: "/static/img/icon-stats-works.png",
-      }),
-    }),
-
-    new emlo.Stats({
-      id: "manifestations",
-      category: "stats",
-      solrCore: "manifestations",
-      renderer: new emlo.StatsRenderer({
-        title: "Versions",
-        titleImage: "/static/img/icon-stats-manifestations.png",
-      }),
-    }),
-
-    new emlo.Stats({
-      id: "images",
-      category: "stats",
-      solrCore: "images",
-      renderer: new emlo.StatsRenderer({
-        title: "Images",
-        titleImage: "/static/img/icon-stats-images.png",
-      }),
-    }),
-
-    new emlo.Stats({
-      id: "comments",
-      category: "stats",
-      solrCore: "comments",
-      renderer: new emlo.StatsRenderer({
-        title: "Comments",
-        titleImage: "/static/img/icon-stats-comments.png",
-      }),
-    }),
-
-    new emlo.Stats({
-      id: "resources",
-      category: "stats",
-      solrCore: "resources",
-      renderer: new emlo.StatsRenderer({
-        title: "Resources",
-        titleImage: "/static/img/icon-stats-related%20resources.png",
+          {
+            title: "Catalogues",
+            titleImage: "/static/img/icon-stats-Catalogues.png",
+            redirectURL:
+              "http://emlo-portal.bodleian.ox.ac.uk/collections/?page_id=480",
+            statKey: "cito_Catalog",
+            tweakCount: 0,
+            upperLimit: 0,
+          },
+          {
+            title: "Letters",
+            titleImage: "/static/img/icon-stats-works.png",
+            redirectURL: "",
+            statKey: "work",
+            tweakCount: 0,
+            upperLimit: 0,
+          },
+          {
+            title: "Versions",
+            titleImage: "/static/img/icon-stats-manifestations.png",
+            redirectURL: "",
+            statKey: "manifestation",
+            tweakCount: 0,
+            upperLimit: 0,
+          },
+          {
+            title: "Images",
+            titleImage: "/static/img/icon-stats-images.png",
+            redirectURL: "",
+            statKey: "image",
+            tweakCount: 0,
+            upperLimit: 48661,
+          },
+          {
+            title: "Comments",
+            titleImage: "/static/img/icon-stats-comments.png",
+            redirectURL: "",
+            statKey: "comment",
+            tweakCount: 0,
+            upperLimit: 0,
+          },
+          {
+            title: "Resources",
+            titleImage: "/static/img/icon-stats-related%20resources.png",
+            redirectURL: "",
+            statKey: "resource",
+            tweakCount: 0,
+            upperLimit: 0,
+          },
+        ],
       }),
     }),
   ];

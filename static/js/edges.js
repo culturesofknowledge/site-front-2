ab069a8e410c46759888ca9b4719ec2e5f2ccdf6
kush.varade.19 <kush.varade.19@gmail.com>
2025-02-12 11:18:12 +0530

    The current selection is updated as per the selected values

--- static/js/edges.js
+++ static/js/edges.js
@@ -1707,11 +1707,14 @@ emlo.SelectedFacetRenderer = class extends edges.Renderer {
         return "Catalogue";
       case "ox_started-ox_year":
         return "Year";
-      case "mail_origin-location" || "pla_ori_name":
+      case "mail_origin-location":
+      case "pla_ori_name":
         return " Origin of letter";
-      case "mail_destination-location" || "pla_des_name":
+      case "mail_destination-location":
+      case "pla_des_name":
         return " Origin of letter";
-      case "dcterms_references-location" || "pla_ment_name":
+      case "dcterms_references-location":
+      case "pla_ment_name":
         return " Places mentioned";
       case "uuid_related":
         return "Any from list";

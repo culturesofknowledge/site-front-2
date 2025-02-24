1d386111a8ecbbfc830740bd28615ef82672759f
kush.varade.19 <kush.varade.19@gmail.com>
2025-02-24 12:26:34 +0530

    Stats count for images and cat is handled

--- views/home.py
+++ views/home.py
@@ -1,12 +1,31 @@
 from flask import Blueprint, render_template, jsonify
 import requests
+import json
 
 home_bp = Blueprint('home', __name__)
 
 
 @home_bp.route('/')
 def home():
-    return render_template('home.jinja2', title="Home")
+    catalouge_data = "static/data/catalouge.json"
+    variables = "static/data/variable.json"
+
+    try:
+        with open(catalouge_data, "r", encoding="utf-8") as file:
+            data = json.load(file)
+        
+        with open(variables, "r", encoding="utf-8") as file:
+            var_data = json.load(file)
+        
+        # Count the length of each array in the JSON
+        catalouge_count = sum(len(value) for value in data.values() if isinstance(value, list))
+        image_limit = var_data.get("image_limit")
+
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        catalouge_count = 0
+        image_limit = 0
+        print(f"Error loading JSON: {e}")
+    return render_template('home.jinja2', title="Home" , catalouge_count = catalouge_count , image_limit=image_limit)
 
 @home_bp.route('/catalogues')
 def load_catalogues():

c165844a8729ef77a1669ecaccb5ca57c9bb356a
kush.varade.19 <kush.varade.19@gmail.com>
2025-03-13 12:41:04 +0530

    Short URL for location is added

--- views/profile.py
+++ views/profile.py
@@ -4,5 +4,4 @@ profile_bp = Blueprint('profile', __name__, url_prefix='/profile')
 
 @profile_bp.route('/<collection>/<id>')
 def profile(collection, id):
-    print(f"Got collection name as: {collection}")
     return render_template('profile.jinja2', title=collection.capitalize(), collection=collection, id=id)

6089a84e7e21b57bfcc7e089a1fb41958305d72f
kush.varade.19 <kush.varade.19@gmail.com>
2025-03-26 07:52:58 +0530

    Alternate names is handled

--- static/js/edges.js
+++ static/js/edges.js
@@ -3735,7 +3735,7 @@ emlo.MultiFieldsRenderer = class extends edges.Renderer {
     if (this.field == "geonames_alternateName") {
       return value
         ? `
-        <p style="white-space:pre-line">
+        <p style="white-space:pre-line;margin-top: -15px;margin-left: 25px;">
            ${edges.util.escapeHtml(value)}
         <p>
       `

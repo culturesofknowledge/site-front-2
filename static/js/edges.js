61525231a038d2c352da317023a7d3617bc84fcd
kush.varade.19 <kush.varade.19@gmail.com>
2025-04-28 09:30:48 +0530

    Taking current href from url for link redirection for short URL

--- static/js/edges.js
+++ static/js/edges.js
@@ -3916,6 +3916,8 @@ emlo.MultiFieldsRenderer = class extends edges.Renderer {
     const currentDomain = window.location.host;
     const result = this.component.results[0];
     const editIdValue = GetRecordID(this.footerType, result);
+    const currentHref = window.location.href;
+
     const shortURL = GenerateShortURL(
       editIdValue,
       this.footerType,
@@ -3972,7 +3974,7 @@ emlo.MultiFieldsRenderer = class extends edges.Renderer {
       }
 
       if (shortURL) {
-        htmlContent += `<li class="footer-links"><a href="${shortURL}">${shortURL}</a></li>`;
+        htmlContent += `<li class="footer-links"><a href="${currentHref}">${shortURL}</a></li>`;
       }
 
       htmlContent += `</ul>`;
@@ -4003,6 +4005,7 @@ emlo.MultiFieldsRenderer = class extends edges.Renderer {
     const currentDomain = window.location.host;
     const result = this.component.results[0];
     const editIdValue = GetRecordID(this.footerType, result);
+    const currentHref = window.location.href;
 
     const shortURL = GenerateShortURL(
       editIdValue,
@@ -4013,7 +4016,7 @@ emlo.MultiFieldsRenderer = class extends edges.Renderer {
     const doc = document.getElementById("short-url-link");
 
     if (doc) {
-      doc.innerHTML = `<a href=${shortURL}> ${shortURL} </a>`;
+      doc.innerHTML = `<a href=${currentHref}> ${shortURL} </a>`;
     }
   }
 

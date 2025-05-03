273f8bf9f16542a9539a8f5c7ac8695acb248e8a
kush.varade.19 <kush.varade.19@gmail.com>
2025-05-03 19:49:33 +0530

    Comment section is converted to new profile temp

---
+++ static/js/profile/commentFrag.js
@@ -0,0 +1,8 @@
+export function _renderCommentProfile() {
+  return `
+        <p>
+            Sorry, this record has not been found. 
+            It may have been deleted as a duplicate.
+        </p>
+    `;
+}

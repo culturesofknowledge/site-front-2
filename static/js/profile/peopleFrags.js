fdab453cb91674dc3fc44f5051ee66ed57eb2eeb
kush.varade.19 <kush.varade.19@gmail.com>
2025-05-04 17:20:01 +0530

    Graph is added to the content section

--- static/js/profile/peopleFrags.js
+++ static/js/profile/peopleFrags.js
@@ -10,13 +10,16 @@ import {
   resourceRelation,
 } from "../../js/helper/helper.js";
 import { getLabel } from "../helper/getFieldLabls.js";
+import PersonChart from "../chart.js";
+
+let personChart;
 
 export function _renderPeopleProfile(profile, tableData, relations) {
   let frag = "";
 
   frag += _renderDetailsSection(profile);
   frag += _renderDateSection(profile);
-  frag += _renderContentStatsSection(profile);
+  frag += _renderContentStatsSection(profile, tableData);
   frag += _renderLettersWritten(profile, tableData);
   frag += _renderLettersRec(profile, tableData);
   frag += _renderLettersMent(profile, tableData);
@@ -144,19 +147,43 @@ function _renderDateSection(profile) {
   }
 }
 
-function _renderContentStatsSection(profile) {
+function _renderContentStatsSection(profile, data) {
+  const graphDataKeys = Object.keys(data);
+
   let sectionFrag = `
     <div class="column profilepart">
       <h3><img src="/static/img/icon-statistics.png" class=""/>Catalogue Statistics</h3>
       <div class="content">${totalLinkingToListWork(profile, "person")}`;
 
-  // Rendering graph
-  let counts = {};
-  // setYearCountsForGraphs(profile, "frbr_creatorOf-work", "", counts);
+  sectionFrag += `
+      <div id="chart">
+        <div class="button-bar">
+          <ul class="button-group unknown" style="${
+            graphDataKeys.length > 1 ? "" : "display:none"
+          }">
+            <li><button id="show_unknown" class="button tiny">Show unknown years</button></li>
+          </ul>
 
-  // PENDING HOLD THE DEVELOPMENT OF GRAPH -
+
+          <ul class="button-group bars" style="${
+            graphDataKeys.length > 1 ? "" : "display:none"
+          }">
+            <li><button id="bars_seperate" class="highlight button tiny">Separate charts</button></li>
+            <li><button id="bars_stacked" class="button tiny">Stack bars</button></li>
+            <li><button id="bars_split" class="button tiny">Split bars</button></li>
+          </ul>
+
+          <ul class="button-group screen">
+            <li><button id="fullscreen" class="button tiny">Full screen</button></li>
+          </ul>
+        </div>
+      </div>
+      <br/>
+    `;
 
   sectionFrag += "</div></div>";
+
+  _renderGraphSection(data);
   return sectionFrag;
 }
 
@@ -293,5 +320,191 @@ function _renderComment(profile, relations) {
     return frag;
   } else {
     return "";
+  }
+}
+
+export function _renderGraphSection(graphData) {
+  const counts = {};
+  for (const key in graphData) {
+    setYearCountsForGraphs(key, graphData[key], counts);
+  }
+
+  let first_and_last = setFirstAndLastYearsForGraphs(counts);
+
+  setYearsWithZeroForGraphs(
+    first_and_last.minYear,
+    first_and_last.maxYear,
+    counts
+  );
+
+  // Sort the years numerically
+  let sortedYears = Object.keys(counts).sort((yearA, yearB) => yearA - yearB);
+
+  let person_data = _toLongFormat(
+    sortedYears.map(function (year) {
+      return [
+        year === "?" ? 9999 : parseInt(year, 10),
+        counts[year].mentioned,
+        counts[year].recipient,
+        counts[year].creator,
+      ];
+    }),
+    []
+  );
+
+  // return person_data;
+
+  // FIX ME: Need a better way to handle this case
+  if (person_data.length > 0) {
+    const checkExist = setInterval(() => {
+      const chartEl = document.getElementById("chart");
+      if (chartEl) {
+        clearInterval(checkExist);
+        personChart = new PersonChart(person_data);
+        _attachEventListeners();
+      } else {
+        console.debug("Waiting in queue for chart to be here");
+      }
+    }, 50);
+  }
+}
+
+function setYearCountsForGraphs(relevantWorksFieldname, data, counts) {
+  // Check if relevantWorksFieldname exists in profile
+  // if (profile.hasOwnProperty(relevantWorksFieldname)) {
+  let relationshipType;
+
+  // Determine relationship type based on the fieldname
+  if (relevantWorksFieldname === "frbr_creatorOf-work") {
+    relationshipType = "creator";
+  } else if (relevantWorksFieldname === "mail_recipientOf-work") {
+    relationshipType = "recipient";
+  } else if (relevantWorksFieldname === "dcterms_isReferencedBy-work") {
+    relationshipType = "mentioned";
+  } else {
+    // Invalid input
+    return;
+  }
+
+  const yearOfWorkFieldname = "ox_started-ox_year";
+
+  // Iterate through the data array
+  data.forEach((item) => {
+    const obj = item;
+
+    let year = "?";
+    if (obj.hasOwnProperty(yearOfWorkFieldname)) {
+      year = obj[yearOfWorkFieldname];
+    }
+
+    // Initialize year in counts if not already present
+    if (!counts.hasOwnProperty(year)) {
+      counts[year] = { creator: 0, recipient: 0, mentioned: 0 };
+    }
+
+    // Increment the value for the current type of work
+    counts[year][relationshipType] += 1;
+  });
+}
+
+function _toLongFormat(d, p, i, z) {
+  for (i = 0, z = d.length; i < z; i++) {
+    p.push({
+      year: d[i][0],
+      mentioned: d[i][1],
+      recipient: d[i][2],
+      creator: d[i][3],
+    });
+  }
+  return p;
+}
+
+function setFirstAndLastYearsForGraphs(counts) {
+  let maxYear = 1;
+  let minYear = 9999;
+
+  // Iterate through the keys of the counts object
+  for (let year in counts) {
+    if (counts.hasOwnProperty(year)) {
+      if (year === "?") {
+        year = 9999;
+      } else {
+        year = parseInt(year); // Convert the year to an integer (since the keys are strings)
+      }
+
+      if (year !== "?" && year !== 9999) {
+        if (year > maxYear) {
+          maxYear = year;
+        }
+        if (year < minYear) {
+          minYear = year;
+        }
+      }
+    }
+  }
+
+  return { minYear, maxYear };
+}
+
+function setYearsWithZeroForGraphs(minYear, maxYear, counts) {
+  let year = minYear;
+
+  while (year < maxYear) {
+    if (!counts.hasOwnProperty(year)) {
+      counts[year] = { creator: 0, recipient: 0, mentioned: 0 };
+    }
+    year++;
+  }
+}
+
+function _attachEventListeners() {
+  const showUnknownButton = document.getElementById("show_unknown");
+  if (showUnknownButton) {
+    showUnknownButton.addEventListener("click", function () {
+      if (personChart) {
+        personChart.unknownShow();
+      }
+      // Handle the click event for the 'Full screen' button
+    });
+  }
+
+  const splitButton = document.getElementById("bars_split");
+  if (splitButton) {
+    splitButton.addEventListener("click", function () {
+      if (personChart) {
+        personChart.switchBars(2);
+      }
+      // Handle the click event for the 'Full screen' button
+    });
+  }
+
+  const seprateButton = document.getElementById("bars_seperate");
+  if (seprateButton) {
+    seprateButton.addEventListener("click", function () {
+      if (personChart) {
+        personChart.switchBars(3);
+      }
+      // Handle the click event for the 'Full screen' button
+    });
+  }
+
+  const stackButton = document.getElementById("bars_stacked");
+  if (stackButton) {
+    stackButton.addEventListener("click", function () {
+      if (personChart) {
+        personChart.switchBars(1);
+      }
+      // Handle the click event for the 'Full screen' button
+    });
+  }
+
+  const fullscreenButton = document.getElementById("fullscreen");
+  if (fullscreenButton) {
+    fullscreenButton.addEventListener("click", function () {
+      if (personChart) {
+        personChart.launchFullScreen();
+      }
+      // Handle the click event for the 'Full screen' button
+    });
   }
 }

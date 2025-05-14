82c7b6aacd2a5d8a5c4372fd08dfe6fc9268112b
kush.varade.19 <kush.varade.19@gmail.com>
2025-05-14 10:18:19 +0530

    Alternative names fixed for inst page

--- static/js/profile/institutionFrag.js
+++ static/js/profile/institutionFrag.js
@@ -36,7 +36,7 @@ function _renderAlternateSection(profile) {
       	<div class="column profilepart">
     				<h3><img src="/static/img/icon-repository.png"/>Alternative names</h3>
 				<div class="content">
-					${profile["geonames_alternateName"].replace("\n", "<br/>") || ""}
+					${profile["geonames_alternateName"].replaceAll("\n", "<br/>") || ""}
 				</div>
 			</div>
     `;

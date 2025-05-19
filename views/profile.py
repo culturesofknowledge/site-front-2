f886bfe22f03621751e0460fd33b46f3443043ab
kush.varade.19 <kush.varade.19@gmail.com>
2025-05-19 11:55:13 +0530

    404 for data not found is added

--- views/profile.py
+++ views/profile.py
@@ -1,7 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from .solr import solr_proxy
 
 profile_bp = Blueprint('profile', __name__, url_prefix='/profile')
 
 @profile_bp.route('/<collection>/<id>')
 def profile(collection, id):
-    return render_template('profile.jinja2', title=collection.capitalize(), collection=collection, id=id)
+    solr_core = "people" if collection == "person" else f"{collection}s"
+
+    solr_url = f'/{solr_core}/select?q=uuid:{id}&wt=json&rows=1&fl=uuid'  # Adjust if needed
+
+    try:
+        response_obj, status_code = solr_proxy(solr_url)
+        
+        if status_code == 200:
+            return render_template('profile.jinja2', title=collection.capitalize(), collection=collection, id=id)
+    except Exception as e:
+        print("Exception occurred while querying Solr:", e)
+
+    return render_template('data_not_found.jinja2', title="Page not found"), 404

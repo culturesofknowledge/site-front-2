132a7207f89921d068ab3918f72d2fc378ea70e2
kush.varade.19 <kush.varade.19@gmail.com>
2025-05-20 10:22:31 +0530

    Bug with 404 redirect handled

--- views/solr.py
+++ views/solr.py
@@ -38,6 +38,38 @@ def solr_proxy(subpath):
         print(f"Got error: {err}")
         return jsonify({"error": str(err)}), 500
     
+# Only used internally 
+def check_profile(collection_name , uuid):
+    try:
+        # Check for empty or None inputs
+        if not collection_name or not uuid:
+            print("Invalid input: collection_name or uuid is empty.")
+            return False
+
+        SOLR_URL = getSolrURL()
+
+        solr_url = f'{collection_name}/select?q=uuid:{uuid}&wt=json&rows=1&fl=uuid'
+        full_url = SOLR_URL + solr_url
+
+        response = requests.get(full_url)
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            data = response.json()
+            if data.get("response", {}).get("numFound", 0) > 0:
+                return True
+            else:
+                return False
+
+        return False
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Got HTTP error: {http_err}")
+        return False
+    except Exception as err:
+        print(f"Got error: {err}")
+        return False
+    
 
 @solr_bp.route('/stats-new', methods=['POST'])
 def fetchStatsNew():

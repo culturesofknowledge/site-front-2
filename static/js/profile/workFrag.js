1e528db90d278495eda58b918040b402ac3015fe
kush.varade.19 <kush.varade.19@gmail.com>
2025-06-23 12:00:41 +0530

    Heading for places mentioned is handled for mention in work profile

--- static/js/profile/workFrag.js
+++ static/js/profile/workFrag.js
@@ -391,7 +391,7 @@ function _renderPlacesSection(profile, relations) {
     if (profile.hasOwnProperty(mentionedField)) {
       placesFrag += `<h4>Mentions</h4>
             <div class="locations mentions">  
-                ${h4RelationshipList(profile, relations, mentionedField)}
+                ${h4RelationshipList(profile, relations, mentionedField, "")}
             `;
 
       placesFrag += `</div>`;

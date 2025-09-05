763325ae3b7f6714a6e3e33f5595bbf5792919ea
Ramakrishna Sakhamuru <rama@cottagelabs.com>
2025-09-05 19:57:30 +0530

    pinned versions

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Flask',
-        'python-dotenv',
+        'Flask==3.1.2',
+        'python-dotenv==1.1.1',
     ],
     entry_points={
         'console_scripts': [

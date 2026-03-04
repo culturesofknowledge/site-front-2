ca04f7bf071210133e31c53d63a78d3a1413e7cd
kush.varade.19 <kush.varade.19@gmail.com>
2026-03-04 10:56:15 +0530

    removed not required modules

--- run.py
+++ run.py
@@ -12,8 +12,10 @@ from views.errors import errors_bp
 from views.solr import solr_bp
 from views.shortURL import shortURL_bp
 from views.redirectUUID import redirect_uuid_bp
+from views.test_runner import test_runner_bp
 from config import Config
 import os
+
 
 def create_app():
     app = Flask(__name__)
@@ -32,14 +34,18 @@ def create_app():
     app.register_blueprint(solr_bp)
     app.register_blueprint(shortURL_bp)
     app.register_blueprint(redirect_uuid_bp)
+    app.register_blueprint(test_runner_bp)
 
     return app
 
+
 app = create_app()
+
 
 def main():
     debug_mode = os.getenv('DEBUG', 'false').lower() == 'true'
     app.run(host='0.0.0.0', port=int(app.config['PORT']), debug=debug_mode)
+
 
 if __name__ == '__main__':
     main()
